4d2d42458fc502bb895499097876a91e93e8c75f
Mifras Gaffoor <81522853+mifrasgaffoor@users.noreply.github.com>
2024-10-17 12:42:40 +0530

    restAssured

--- src/test/java/cookiesAndHeaders/HeadersDemo.java
+++ src/test/java/cookiesAndHeaders/HeadersDemo.java
@@ -1,4 +1,31 @@
 package cookiesAndHeaders;
+import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 public class HeadersDemo {
+
+
+    @Test
+
+    void testHeader(){
+        given()
+
+                .when()
+                .get("https://www.google.com/")
+
+                .then()
+                .statusCode(200)
+                .header("Content-Type" ,"text/html; charset=ISO-8859-1")
+                .and()
+                .header("Expires","-1")
+                .and()
+                .header("Server" ,"gws")
+                .log().all();
+
+    }
+
+
+
 }
